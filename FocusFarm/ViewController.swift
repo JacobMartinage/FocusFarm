74f9ae3fc9d19edf93ffb5fc8f2285fc55620442
JacobMartinage <62039158+JacobMartinage@users.noreply.github.com>
2023-11-01 17:37:19 -0400

    Initial Commit

---
+++ FocusFarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FocusFarm
+//
+//  Created by Jacob Martinage on 11/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
